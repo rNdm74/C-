776a05eb332fbe5d882a4ae9ceb2d53c9ff75dc7
rNdm <rndm@windowslive.com>
2014-03-05 22:37:48 +1300

    Initial commit

---
+++ Stack Unit Testing/Stack Unit Testing Tests/StackTest.cs
@@ -0,0 +1,401 @@
+﻿using Stack_Unit_Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Stack_Unit_Testing_Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for StackTest and is intended
+    ///to contain all StackTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class StackTest
+    {        
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        #region Count()
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void CountTestDefault()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+
+            int expected = 3; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.Count();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void CountTestEmptyStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            
+            int expected = 0; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.Count();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void CountTestFullStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+
+            int expected = 5; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.Count();
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+        #region IsEmpty()
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void IsEmptyTestStackEmpty()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+
+            bool expected = true; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.IsEmpty();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void IsEmptyTestStackHasItem()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.IsEmpty();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void IsEmptyTestAfterItemsPopped()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            item = target.Pop();
+
+            bool expected = true; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.IsEmpty();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void IsEmptyTestAfterFillingStackThenPoppingStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            item = target.Pop();
+            item = target.Pop();
+            item = target.Pop();
+            item = target.Pop();
+            item = target.Pop();
+
+            bool expected = true; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.IsEmpty();
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+                
+        #region Peek()
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PeekTestEmptyStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+
+            string expected = null; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Peek();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PeekTestFullStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+
+            string expected = "Hello"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Peek();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PeekTestCheckDoesntRemoveItem()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            item = target.Peek();
+
+            int expected = 1; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.Count();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PeekTestHelperDefault()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+
+            string expected = "Hello"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Peek();
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+                        
+        #region Pop()    
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PopTestDefault()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            item = target.Pop();
+
+            int expected = 3; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PopTestEmptyStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            //string item; // TODO: Initialize to an appropriate value
+            //item = target.Pop();
+
+            string expected = null; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Pop();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PopTestFullStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            item = target.Pop();
+
+            int expected = 4; // TODO: Initialize to an appropriate value
+            int actual;
+            actual = target.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PopTestEmptyStackThenPush()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = target.Pop(); // TODO: Initialize to an appropriate value                
+            item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+
+            string expected = "Hello"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Peek();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PopTestEmptyStackMultipleTimes()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = target.Pop(); // TODO: Initialize to an appropriate value                
+            item = target.Pop();
+            item = target.Pop();
+            item = target.Pop();
+
+            //Assert.AreEqual(expected, actual);
+        }
+        #endregion
+                
+        #region Push()
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PushTestEmptyStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+
+            string expected = "Hello"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Pop();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void PushTestItemsInStack()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+
+            string expected = "Hello"; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.Pop();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.StackOverflowException))]
+        public void PushTestPushStackIsFullException()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+            string item = "Hello"; // TODO: Initialize to an appropriate value
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+            target.Push(item);
+        }
+        #endregion
+                
+        #region Constructor
+        [TestMethod()]
+        [DeploymentItem("Stack Unit Testing.exe")]
+        public void StackConstructorTest()
+        {
+            int param0 = 5; // TODO: Initialize to an appropriate value
+            Stack_Accessor<string> target = new Stack_Accessor<string>(param0); // TODO: Initialize to an appropriate value
+
+            Assert.IsNotNull(target);
+        }
+        #endregion
+    }
+}
---
+++ Stack Unit Testing/Stack Unit Testing/Stack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack_Unit_Testing
+{
+    class Stack<T>
+    {
+        private readonly int m_Size;
+        private int m_StackPointer = 0;
+        private T[] m_Items;
+
+        public Stack(int size) 
+        {
+            m_Size = size;
+            m_Items = new T[m_Size];
+        }
+
+        public void Push(T item)
+        {
+            // Check is stack is full
+            if (m_StackPointer >= m_Size)
+                throw new StackOverflowException();
+            
+            // Add item to stack
+            m_Items[m_StackPointer] = item;
+            // Move pointer to next available position in stack
+            m_StackPointer++;
+        }
+
+        public T Pop()
+        {
+            // Point to last added item in stack
+            m_StackPointer--;
+                        
+            if (m_StackPointer >= 0) // Stack has items
+            {
+                // Copy item to temp
+                T m_MostRecentItem = m_Items[m_StackPointer]; 
+                // Remove item from stack
+                m_Items[m_StackPointer] = default(T);
+                // Return last added item in stack
+                return m_MostRecentItem;
+            }
+            else // Stack empty : m_StackPointer = -1
+            {
+                m_StackPointer = 0;
+                // Returns the default value of a type
+                return default(T);
+            }
+        }
+
+        public T Peek()
+        {
+            // Point to last added item in stack
+            m_StackPointer--;
+
+            if (m_StackPointer >= 0) // Stack has items
+            {
+                // Return last added item in stack
+                return m_Items[m_StackPointer];
+            }
+            else // Stack empty : m_StackPointer = -1
+            {
+                m_StackPointer = 0;
+                // Returns the default value of a type
+                return default(T);
+            }
+        }
+
+        public int Count()
+        {
+            int m_Count = 0;
+
+            // Count all items in stack
+            while (m_Count < m_Items.Length && m_Items[m_Count] != null)
+                m_Count++;
+
+            return m_Count;
+        }
+        
+        public bool IsEmpty()
+        {
+            // This doesnt feel right
+            return m_StackPointer == 0;
+        }
+    }
+}
