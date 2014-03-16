98a464317ddbf9e5a5d886fa8f76b64a48dcd001
rNdm <rndm@windowslive.com>
2014-03-17 07:19:55 +1300

    Initial commit

--- Clocks/Clocks/Form1.Designer.cs
+++ Clocks/Clocks/Form1.Designer.cs
@@ -28,8 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.analogClock = new AnalogClockControl.AnalogClock();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbAnalogue = new System.Windows.Forms.RadioButton();
+            this.rbDigital = new System.Windows.Forms.RadioButton();
+            this.lDigitalTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tClock = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // analogClock
@@ -37,33 +44,95 @@
             this.analogClock.Draw1MinuteTicks = true;
             this.analogClock.Draw5MinuteTicks = true;
             this.analogClock.HourHandColor = System.Drawing.Color.DarkMagenta;
-            this.analogClock.Location = new System.Drawing.Point(411, 231);
+            this.analogClock.Location = new System.Drawing.Point(280, 28);
             this.analogClock.MinuteHandColor = System.Drawing.Color.Green;
             this.analogClock.Name = "analogClock";
             this.analogClock.SecondHandColor = System.Drawing.Color.Red;
-            this.analogClock.Size = new System.Drawing.Size(150, 150);
+            this.analogClock.Size = new System.Drawing.Size(197, 197);
             this.analogClock.TabIndex = 0;
             this.analogClock.TicksColor = System.Drawing.Color.Black;
+            this.analogClock.Visible = false;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.rbDigital);
+            this.groupBox1.Controls.Add(this.rbAnalogue);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.Size = new System.Drawing.Size(232, 149);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Clock Type";
+            // 
+            // rbAnalogue
+            // 
+            this.rbAnalogue.AutoSize = true;
+            this.rbAnalogue.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAnalogue.Location = new System.Drawing.Point(45, 88);
+            this.rbAnalogue.Name = "rbAnalogue";
+            this.rbAnalogue.Size = new System.Drawing.Size(155, 37);
+            this.rbAnalogue.TabIndex = 0;
+            this.rbAnalogue.TabStop = true;
+            this.rbAnalogue.Text = "Analogue";
+            this.rbAnalogue.UseVisualStyleBackColor = true;
+            // 
+            // rbDigital
+            // 
+            this.rbDigital.AutoSize = true;
+            this.rbDigital.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDigital.Location = new System.Drawing.Point(45, 45);
+            this.rbDigital.Name = "rbDigital";
+            this.rbDigital.Size = new System.Drawing.Size(115, 37);
+            this.rbDigital.TabIndex = 1;
+            this.rbDigital.TabStop = true;
+            this.rbDigital.Text = "Digital";
+            this.rbDigital.UseVisualStyleBackColor = true;
+            this.rbDigital.CheckedChanged += new System.EventHandler(this.rbDigital_CheckedChanged);
+            // 
+            // lDigitalTime
+            // 
+            this.lDigitalTime.AutoSize = true;
+            this.lDigitalTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDigitalTime.Location = new System.Drawing.Point(274, 105);
+            this.lDigitalTime.Name = "lDigitalTime";
+            this.lDigitalTime.Size = new System.Drawing.Size(142, 33);
+            this.lDigitalTime.TabIndex = 2;
+            this.lDigitalTime.Text = "DateTime";
+            this.lDigitalTime.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Turn Clock On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tClock
+            // 
+            this.tClock.Interval = 1000;
+            this.tClock.Tick += new System.EventHandler(this.tClock_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 393);
+            this.ClientSize = new System.Drawing.Size(503, 253);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lDigitalTime);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.analogClock);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +140,11 @@
 
         private AnalogClockControl.AnalogClock analogClock;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbDigital;
+        private System.Windows.Forms.RadioButton rbAnalogue;
+        private System.Windows.Forms.Label lDigitalTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer tClock;
     }
 }
 
--- Clocks/Clocks/Form1.cs
+++ Clocks/Clocks/Form1.cs
@@ -11,9 +11,122 @@ namespace Clocks
 {
     public partial class Form1 : Form
     {
+        private IClock clock;
+        private bool clockOn;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rbDigital.Checked)
+            {
+                clock.hide();
+                clock = new Digital(lDigitalTime);
+                clock.show();
+            }
+            else
+            {
+                clock.hide();
+                clock = new Analogue(analogClock);
+                clock.show();
+            }
+
+            clockOn = !clockOn;
+
+            if (clockOn)
+                clock.show();
+            else
+                clock.hide();
+        }
+
+        interface IClock
+        {
+            void show();
+            void hide();
+        }
+
+        public class Digital : IClock
+        {
+            Label digitalClock;
+
+            public Digital(Label digitalClock) 
+            {
+                this.digitalClock = digitalClock;
+            }
+
+            void IClock.show()
+            {
+                digitalClock.Visible = true;
+            }
+
+            void IClock.hide()
+            {
+                digitalClock.Visible = false;
+            }
+        }
+        public class Analogue : IClock
+        {
+            AnalogClockControl.AnalogClock analogueClock;
+
+            public Analogue(AnalogClockControl.AnalogClock analogueClock) 
+            {
+                this.analogueClock = analogueClock;
+            }
+
+            void IClock.show()
+            {
+                analogueClock.Visible = true;
+            }
+
+            void IClock.hide()
+            {
+                analogueClock.Visible = false;
+            }
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            DateTime date1 = DateTime.Now;
+
+            lDigitalTime.Text = date1.ToString("hh:mm:ss tt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tClock.Enabled = true;
+
+            if (rbDigital.Checked)
+            {
+                clock = new Digital(lDigitalTime);
+            }
+            else
+            {
+                clock = new Analogue(analogClock);
+            }
+        }
+
+        private void rbDigital_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rButton = (RadioButton)sender;
+
+            if(clockOn)
+            {
+                if (rButton.Checked)
+                {
+                    clock.hide();
+                    clock = new Digital(lDigitalTime);
+                    clock.show();
+                }
+                else
+                {
+                    clock.hide();
+                    clock = new Analogue(analogClock);
+                    clock.show();
+                }
+            }                      
         }
     }
 }
